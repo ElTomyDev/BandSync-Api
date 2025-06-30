f374dff7397cc5dc86d3c548dd1785e62b0fb758
ElTomyDev <eltomyprog@gmail.com>
2025-06-30 01:23:58 -0300

    add more comments and move lifespan function

--- app/main.py
+++ app/main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from app.database import connect_mongo_database, close_mongo_database
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Esto se ejecuta antes de que inicie la API
-    await connect_mongo_database()
-    yield
-    # Esto se ejecuta antes de que se cierre la API
-    await close_mongo_database()
+from app.configs.lifespan import lifespan
 
 app = FastAPI(title="BandSync API", lifespan=lifespan)
+
 
 
