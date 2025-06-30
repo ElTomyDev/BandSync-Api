f2dc666e4cbe2bf6e9cacbeabb63f1e2dd22f9b6
ElTomyDev <eltomyprog@gmail.com>
2025-06-30 15:15:56 -0300

    Update database setings and create depens

---
+++ app/db/database_provider.py
@@ -0,0 +1,8 @@
+from fastapi import Request, Depends
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
+def get_mongo_db(request: Request) -> AsyncIOMotorDatabase:
+    """
+    Retorna una instancia de la base de datos guardada anteriormente al momento de conectarla
+    """
+    return request.app.state.db
