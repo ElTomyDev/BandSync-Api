0559408d559a771c418cecea7a1f1b2a2de0fe6b
ElTomyDev <eltomyprog@gmail.com>
2025-06-30 16:12:23 -0300

    Create register user schema

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+
+class RegisterUserSchema(BaseModel):
+    name: str = Field(..., min_length=2, max_length=50)
+    lastname: str = Field(..., min_length=2, max_length=50)
+    username: str = Field(..., min_length=3, max_length=20)
+    description: str | None = Field(..., max_length=250)
+    
