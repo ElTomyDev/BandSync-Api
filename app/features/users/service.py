872138f9b34256dd62266847018307406719fad1
ElTomyDev <eltomyprog@gmail.com>
2025-07-01 21:14:21 -0300

    Update service.py

--- app/features/users/service.py
+++ app/features/users/service.py
@@ -6,7 +6,7 @@ from fastapi import Request
 class UserService:
     def __init__(self, request: Request):
         self.db: AsyncIOMotorDatabase = request.app.state.db
-        
+    
     async def create_user(self, user_data: UserRegisterSchema) -> UserResponseSchema:
         user = UserModel(**user_data.model_dump()).model_dump()
 
