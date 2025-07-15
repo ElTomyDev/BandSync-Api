6b3f2effcb12d5cc08fda0274d4173d777574f8c
ElTomyDev <eltomyprog@gmail.com>
2025-07-15 18:23:39 -0300

    create password schema

---
+++ app/features/users/features/user_password/schema.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class UserPasswordCreateSchema(BaseModel):
+    password: str = Field(..., min_length=6, max_length=255)
+
+class UserPasswordUpdateSchema(BaseModel):
+    last_password: str = Field(...)
+    new_password: str = Field(..., min_length=6, max_length=255)
+
+class UserPasswordResponseSchema(BaseModel):
+    id: str = Field(alias="_id")
+    id_user: str
+    create_date: datetime
+    
+    model_config = ConfigDict(
+        arbitrary_types_allowed = True,
+        serialize_by_alias=True)
+    
