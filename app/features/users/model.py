a01edf5cd19f49920390d52f8da94a2ec2363d58
ElTomyDev <eltomyprog@gmail.com>
2025-07-15 17:53:22 -0300

    Update model.py

--- app/features/users/model.py
+++ app/features/users/model.py
@@ -8,19 +8,19 @@ class UserModel(BaseModel):
     id: ObjectId = Field(default_factory=lambda: ObjectId(), alias="_id")
     location_id: ObjectId = Field(default=None)
     social_id: ObjectId = Field(default=None)
-    image_url: str | None = None
-    musical_role: MusicalRoles = MusicalRoles.NONE
+    image_url: str | None = Field(default=None)
+    musical_role: MusicalRoles = Field(default=MusicalRoles.NONE)
     name: str
     lastname: str
     username: str
-    description: str | None = None
+    description: str | None = Field(default=None)
     create_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    last_connection: datetime | None = None
-    account_state: AccountStates = AccountStates.ACTIVE
-    phone_number: str | None = None
-    last_failed_login: datetime | None = None
-    failed_login_attempts: int = 0
-    find_bands: bool = False
+    last_connection: datetime | None = Field(default=None)
+    account_state: AccountStates = Field(default=AccountStates.ACTIVE)
+    phone_number: str | None = Field(default=None)
+    last_failed_login: datetime | None = Field(default=None)
+    failed_login_attempts: int = Field(default=0)
+    find_bands: bool = Field(default=False)
     
     model_config = ConfigDict(
         arbitrary_types_allowed = True,
