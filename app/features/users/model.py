58c911f1e8e90753cc678158d4dfd193d173cbc7
ElTomyDev <eltomyprog@gmail.com>
2025-08-11 11:51:51 -0300

    Crea un modelo para el login del usuario y lo incorpora en el modelo del usuario original

--- app/features/users/model.py
+++ app/features/users/model.py
@@ -7,27 +7,26 @@ from bson.objectid import ObjectId
 from app.features.locations.model import LocationModel
 from app.features.social_links.model import SocialLinksModel
 from app.features.users.email_auth.model import EmailAuthModel
+from app.features.users.login_auth.model import LoginAuthModel
 from app.features.users.password_auth.model import PasswordAuthModel
 
 class UserModel(BaseModel):
     id: ObjectId = Field(default_factory=lambda: ObjectId(), alias="_id")
-    location: LocationModel
-    social_links: SocialLinksModel
     image_url: str | None = Field(default=None)
-    musical_role: MusicalRoles = Field(default=MusicalRoles.NONE)
     name: str
     lastname: str
     username: str
-    password_auth: PasswordAuthModel
-    email_auth: EmailAuthModel
-    description: str | None = Field(default=None)
-    create_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    last_connection: datetime | None = Field(default=None)
-    account_state: AccountStates = Field(default=AccountStates.ACTIVE)
     phone_number: str | None = Field(default=None)
-    last_failed_login: datetime | None = Field(default=None)
-    failed_login_attempts: int = Field(default=0)
+    description: str | None = Field(default=None)
     find_bands: bool = Field(default=False)
+    musical_role: MusicalRoles = Field(default=MusicalRoles.NONE)
+    account_state: AccountStates = Field(default=AccountStates.ACTIVE)
+    password_auth: PasswordAuthModel
+    location: LocationModel
+    social_links: SocialLinksModel
+    email_auth: EmailAuthModel
+    login_auth: LoginAuthModel
+    create_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     
     model_config = ConfigDict(
         arbitrary_types_allowed = True,
